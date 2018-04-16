b609afeceff6ab27a10eaba75018b0a720e8481d
MIJAEL888 <mijael888@gmail.com>
2018-04-16 12:08:35 -0500

    Init commit

---
+++ src/main/java/com/pichangas/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package com.pichangas.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Pichangas.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
